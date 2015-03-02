45d8cdfb55ffff20e66eef99d01925c7e6061827
Sheldon <webtestingrun15@users.noreply.github.com>
2015-03-02 13:30:09 -0500

    add remove for class for main

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 $(function() {
   var $menu = $('#menu');
+  var $main = $('main');
   var $drawer = $('.nav');
 
   $menu.click(function(event) {
     $drawer.toggleClass('open');
+    event.stopPropagation();
+
+  });
+  $main.click(function(event) {
+    $drawer.removeClass('open');
     event.stopPropagation();
   });
 });
